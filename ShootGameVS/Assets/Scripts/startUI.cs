49983a4a399c3fa9704dc708057e5a59417c5c57
KarbyLee179 <forleer179@gmail.com>
2023-09-21 11:21:16 +0900

    good

---
+++ ShootGameVS/Assets/Scripts/startUI.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class startUI : MonoBehaviour
+{
+    [SerializeField] GameObject option;
+
+    void Start()
+    {
+        option.SetActive(false);
+    }
+    
+    public void optionOpen()
+    {
+        option.SetActive(true);
+    }
+
+    public void optionClose()
+    {
+        option.SetActive(false);
+    }
+
+    public void gameExit()
+    {
+        Application.Quit();
+    }
+
+    public void gameStart()
+    {
+        SceneManager.LoadScene("waitingRoom");
+    }
+}
